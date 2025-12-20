b9df5d8b56344302c7014a70edcef09312129c54
anishav473 <you@example.com>
2025-12-20 18:42:49 +0530

    ok

---
+++ web1/jss/script.js
@@ -0,0 +1,21 @@
+// Mobile Menu Toggle
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth Scrolling for links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        // Close mobile menu after clicking
+        if(navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
